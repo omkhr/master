1cee0c317d4004747abadcf9ba17489d4ea146f5
omkhr <45929538+omkhr@users.noreply.github.com>
2018-12-19 10:12:47 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class EvenOdd {
+	public static void main(String[] args)
+	{
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int a=s.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("Number is even");
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+			
+	}
+
+}
